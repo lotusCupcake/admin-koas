fe4c2d4ae3e10871d97b88bbeb8a29d9a688a482
fikriansari <fikriansari.mfa@gmail.com>
2021-12-27 11:57:47 +0700

    auth login

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -19,6 +19,9 @@ class Filters extends BaseConfig
         'csrf'     => CSRF::class,
         'toolbar'  => DebugToolbar::class,
         'honeypot' => Honeypot::class,
+        'login'      => \Myth\Auth\Filters\LoginFilter::class,
+        'role'       => \Myth\Auth\Filters\RoleFilter::class,
+        'permission' => \Myth\Auth\Filters\PermissionFilter::class,
     ];
 
     /**
@@ -29,8 +32,8 @@ class Filters extends BaseConfig
      */
     public $globals = [
         'before' => [
-            // 'honeypot',
-            // 'csrf',
+            'honeypot',
+            'login'
         ],
         'after' => [
             'toolbar',
@@ -58,5 +61,7 @@ class Filters extends BaseConfig
      *
      * @var array
      */
-    public $filters = [];
+    public $filters = [
+        // 'login' => ['before' => ['acara']],
+    ];
 }
