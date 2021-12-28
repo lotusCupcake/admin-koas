0b4aa467525cdb477a2cbe70c4e0d9bd555bc273
fikriansari <fikriansari.mfa@gmail.com>
2021-12-28 09:14:37 +0700

    fix view

--- app/Controllers/Maintenance.php
+++ app/Controllers/Maintenance.php
@@ -9,7 +9,7 @@ class Maintenance extends BaseController
         $data = [
             'title' => "Maintenance",
             'appName' => "KOAS",
-            'breadcrumb' => ['Home', 'Mainteance'],
+            'breadcrumb' => ['Home', 'Maintenance'],
             'menu' => $this->fetchMenu()
         ];
         return view('pages/maintenance', $data);
