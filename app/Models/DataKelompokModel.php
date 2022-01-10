7d470fc3e5b2dac7f0e34552472d6156d35b81f9
Sukrianto IT <sukriazuki0@gmail.com>
2022-01-10 15:13:45 +0700

    Fix Data Kelompok

--- app/Models/DataKelompokModel.php
+++ app/Models/DataKelompokModel.php
@@ -15,10 +15,6 @@ class DataKelompokModel extends Model
     {
         $builder = $this->db->table('kelompok');
         $builder->select('*,(select count(*) from kelompok_detail where kelompokDetKelompokId = kelompok.kelompokId)as jumlahPartisipan');
-<<<<<<< HEAD
-        $builder->join('dosen_kelompok', 'dosen_kelompok.dosenKelompokId = kelompok.kelompokDosenKelompokId', 'LEFT');
-=======
->>>>>>> 25cbc850fcbc727b15f166aac50d518dc6e2ad77
         $kelompok = $builder->get();
         return $kelompok;
     }
