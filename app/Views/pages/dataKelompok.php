e57e9c0fa443f6ef8f6a8ac836ae57c526a76064
fikriansari <fikriansari.mfa@gmail.com>
2022-01-17 12:34:27 +0700

    fix view

--- app/Views/pages/dataKelompok.php
+++ app/Views/pages/dataKelompok.php
@@ -252,7 +252,7 @@
             </div>
           </div>
           <div class="modal-footer bg-whitesmoke br">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
           </div>
         </form>
       </div>
