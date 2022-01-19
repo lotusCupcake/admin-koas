648c2e1e4b3f6280a12747fd1cf91751ef33bc41
fikriansari <fikriansari.mfa@gmail.com>
2022-01-19 16:41:17 +0700

    paginate dan restict view

--- app/Controllers/FollowUp.php
+++ app/Controllers/FollowUp.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\FollowUpModel;
 use App\Models\UsersModel;
+use App\Models\DosenPembimbingModel;
 
 class FollowUp extends BaseController
 {
@@ -11,22 +12,34 @@ class FollowUp extends BaseController
     public function __construct()
     {
         $this->followUpModel = new FollowUpModel();
+        $this->dosenPembimbingModel = new DosenPembimbingModel();
     }
     public function index()
     {
         $currentPage = $this->request->getVar('page_followUp') ? $this->request->getVar('page_followUp') : 1;
         $keyword = $this->request->getVar('keyword');
 
-        $this->usersModel = new UsersModel();
-        $usr = $this->usersModel->getSpecificUser(['users.id' => user()->id])->getResult()[0]->name;
-        $where = null;
-        if ($usr == 'Dosen') {
-            $where = array('dosen_pembimbing.dopingId' => user()->id);
-        }
-        if ($keyword) {
-            $followUp = $this->followUpModel->getFollowUpSearch($keyword, $where);
+        if (in_groups('Koordik')) {
+            $rs = $this->dosenPembimbingModel->getSpecificDosen(['dopingEmail' => user()->email])->get()->getResult()[0]->dopingRumkitId;
+            dd($rs);
+            if ($keyword) {
+                $followUp = $this->followUpModel->getFollowUpSearch($keyword, ['dosen_pembimbing.dopingRumkitId' => $rs]);
+            } else {
+                // dd($this->followUpModel->getFollowUp(['dosen_pembimbing.dopingRumkitId' => $rs])->get()->getResult());
+                $followUp = $this->followUpModel->getFollowUp(['dosen_pembimbing.dopingRumkitId' => $rs]);
+            };
         } else {
-            $followUp = $this->followUpModel->getFollowUp($where);
+            $this->usersModel = new UsersModel();
+            $usr = $this->usersModel->getSpecificUser(['users.id' => user()->id])->getResult()[0]->name;
+            $where = null;
+            if ($usr == 'Dosen') {
+                $where = array('dosen_pembimbing.dopingId' => user()->id);
+            }
+            if ($keyword) {
+                $followUp = $this->followUpModel->getFollowUpSearch($keyword, $where);
+            } else {
+                $followUp = $this->followUpModel->getFollowUp($where);
+            }
         }
         $data = [
             'title' => "Follow Up",
