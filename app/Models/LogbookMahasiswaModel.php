879cb4ef72d0bf2aea39a4b59044dd278b04e192
fikriansari <fikriansari.mfa@gmail.com>
2022-01-22 08:26:53 +0700

    fix controller

--- app/Models/LogbookMahasiswaModel.php
+++ app/Models/LogbookMahasiswaModel.php
@@ -51,7 +51,7 @@ class LogbookMahasiswaModel extends Model
     {
         $builder = $this->table('logbook');
         $builder->join('rumkit_detail', 'rumkit_detail.rumkitDetId = logbook.logbookRumkitDetId', 'LEFT');
-        $builder->join('dosen_pembimbing', 'dosen_pembimbing.dopingId = logbook.logbookDopingId', 'LEFT');
+        $builder->join('dosen_pembimbing', 'dosen_pembimbing.dopingEmail = logbook.logbookDopingEmail', 'LEFT');
         $builder->join('kelompok_detail', 'kelompok_detail.kelompokDetNim = logbook.logbookNim', 'LEFT');
         $builder->join('kelompok', 'kelompok.kelompokId = kelompok_detail.kelompokDetKelompokId', 'LEFT');
         $builder->join('rumkit', 'rumkit.rumahSakitId = rumkit_detail.rumkitDetRumkitId', 'LEFT');
