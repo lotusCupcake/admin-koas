d675579a0b4369485d910f84a45c8abf9ef53da9
irvan <august.ivan20@gmail.com>
2022-02-14 19:53:24 +0700

    clean stas

--- app/Models/JadwalKegiatanModel.php
+++ app/Models/JadwalKegiatanModel.php
@@ -8,21 +8,16 @@ class JadwalKegiatanModel extends Model
 {
     protected $table = 'jadwal';
     protected $primaryKey = 'jadwalId';
-    // protected $allowedFields = ['jadwalRumkitDetId', 'jadwalKelompokId', 'jadwalJamMasuk', 'jadwalTanggalMulai', 'jadwalTanggalSelesai', 'jadwalJamKeluar'];
     protected $allowedFields = ['jadwalRumkitDetId', 'jadwalKelompokId', 'jadwalJamMasuk', 'jadwalTanggalMulai', 'jadwalTanggalSelesai', 'jadwalJamKeluar', 'jadwalJumlahWeek'];
     protected $returnType = 'object';
 
     public function show_Jadwal_Kegiatan()
     {
         $builder = $this->table('jadwal');
-        // $builder->select('DISTINCT (jadwal.jadwalId) AS jadwalId, rumkit_detail.*, rumkit.*, stase.*, CONCAT(jadwal.jadwalJamMasuk," - ",jadwal.jadwalJamKeluar," WIB") AS jadwalJam, kelompok.*, date(FROM_UNIXTIME( jadwal.jadwalTanggalMulai / 1000 )) AS jadwalTanggalMulai, date(FROM_UNIXTIME( jadwal.jadwalTanggalSelesai / 1000 )) AS jadwalTanggalSelesai,jadwal.jadwalJumlahWeek,jadwal.jadwalJamMasuk,jadwal.jadwalJamKeluar,(select  GROUP_CONCAT( DISTINCT CONCAT( " ", kelompok_detail.kelompokDetNama, " (" ), CONCAT(kelompok_detail.kelompokDetNim,")") ORDER BY kelompok_detail.kelompokDetId ASC )  from kelompok_detail where kelompok_detail.kelompokDetKelompokId=jadwal.jadwalKelompokId
-        // GROUP BY kelompok_detail.kelompokDetKelompokId) AS Mahasiswa');
         $builder->join('kelompok', 'kelompok.kelompokId = jadwal.jadwalKelompokId', 'LEFT');
-        $builder->join('kelompok_detail', 'kelompok_detail.kelompokDetKelompokId = kelompok.kelompokId', 'LEFT');
         $builder->join('rumkit_detail', 'rumkit_detail.rumkitDetId = jadwal.jadwalRumkitDetId', 'LEFT');
         $builder->join('rumkit', 'rumkit.rumahSakitId = rumkit_detail.rumkitDetRumkitId', 'LEFT');
         $builder->join('stase', 'stase.staseId = rumkit_detail.rumkitDetStaseId', 'LEFT');
-        $builder->groupBy(['kelompok_detail.kelompokDetKelompokId', 'jadwal.jadwalRumkitDetId']);
         $builder->orderBy('jadwal.jadwalId', 'DESC');
         return $builder;
     }
@@ -37,15 +32,11 @@ class JadwalKegiatanModel extends Model
     public function show_Jadwal_KegiatanSearch($keyword)
     {
         $builder = $this->table('jadwal');
-        // $builder->select('DISTINCT (jadwal.jadwalId) AS jadwalId, rumkit_detail.*, rumkit.*, stase.*, CONCAT(jadwal.jadwalJamMasuk," - ",jadwal.jadwalJamKeluar," WIB") AS jadwalJam, kelompok.*, date(FROM_UNIXTIME( jadwal.jadwalTanggalMulai / 1000 )) AS jadwalTanggalMulai, date(FROM_UNIXTIME( jadwal.jadwalTanggalSelesai / 1000 )) AS jadwalTanggalSelesai,jadwal.jadwalJumlahWeek,jadwal.jadwalJamMasuk,jadwal.jadwalJamKeluar,(select  GROUP_CONCAT( DISTINCT CONCAT( " ", kelompok_detail.kelompokDetNama, " (" ), CONCAT(kelompok_detail.kelompokDetNim,")") ORDER BY kelompok_detail.kelompokDetId ASC )  from kelompok_detail where kelompok_detail.kelompokDetKelompokId=jadwal.jadwalKelompokId
-        // GROUP BY kelompok_detail.kelompokDetKelompokId) AS Mahasiswa');
         $builder->join('kelompok', 'kelompok.kelompokId = jadwal.jadwalKelompokId', 'LEFT');
-        $builder->join('kelompok_detail', 'kelompok_detail.kelompokDetKelompokId = kelompok.kelompokId', 'LEFT');
         $builder->join('rumkit_detail', 'rumkit_detail.rumkitDetId = jadwal.jadwalRumkitDetId', 'LEFT');
         $builder->join('rumkit', 'rumkit.rumahSakitId = rumkit_detail.rumkitDetRumkitId', 'LEFT');
         $builder->join('stase', 'stase.staseId = rumkit_detail.rumkitDetStaseId', 'LEFT');
-        $builder->like('kelompok_detail.kelompokDetNama', $keyword);
-        $builder->orLike('kelompok_detail.kelompokDetNim', $keyword);
+        $builder->like('kelompok.kelompokNama', $keyword);
         $builder->orLike('stase.staseNama', $keyword);
         $builder->orLike('rumkit.rumahSakitNama', $keyword);
         $builder->orderBy('jadwal.jadwalId', 'DESC');
