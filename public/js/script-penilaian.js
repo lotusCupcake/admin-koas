97e97058cbe9acc24de2806165877b1d259f9547
archy7seven <august.ivan20@gmail.com>
2022-02-25 08:11:30 +0700

    menambah nilai huruf

--- public/js/script-penilaian.js
+++ public/js/script-penilaian.js
@@ -1,25 +1,34 @@
 $(document).ready(function () {
   var jml = 0;
   
-  $('.btn-LapKasus').click(function () {
+  $('.icon-left.btn-info').click(function () {
     jml = $('.LapKasus').length;
+    var getClass = this.className;
+    getClass = getClass.split(' ');
+    komp = getClass[4];
+    komp = komp.split('-');
+    // console.log(komp[1]);
+    createEvent(komp[1]);
   });
 
-  $('input[type=radio].r-LapKasus').change(function () { 
-    var total = 0;
-    
-    for (let i = 1; i <= jml; i++) {
-      var nilai = parseInt($('.val-LapKasus' + i + ':checked').val());
-      var bobot = parseInt($('.val-LapKasus' + i + ':checked').data('kompbobot'));
-      var skorMax = parseInt($('.val-LapKasus' + i + ':checked').data('skormax'));
+  function createEvent(komp) {
+    $('input[type=radio].r-'+komp).change(function () { 
+      var total = 0;
       
-      (isNaN(nilai)) ? nilai = 0 : nilai = (nilai*bobot)/skorMax;
-      total = total + nilai;
-    }
-    
-      callGrade(total)
-    
-  });
+      for (let i = 1; i <= jml; i++) {
+        var nilai = parseInt($('.val-'+komp + i + ':checked').val());
+        var bobot = parseInt($('.val-'+komp + i + ':checked').data('kompbobot'));
+        var skorMax = parseInt($('.val-'+komp+ i + ':checked').data('skormax'));
+        
+        (isNaN(nilai)) ? nilai = 0 : nilai = (nilai*bobot)/skorMax;
+        total = total + nilai;
+      }
+      
+        callGrade(total)
+      
+    });
+  }
+  
 
   function callGrade(total) {
     $.ajax({
@@ -29,7 +38,7 @@ $(document).ready(function () {
           nilai: total
         }, 
         success: function (response) {
-          console.log(response);
+          // console.log(response);
           $('.grade').html('Nilai Huruf : '+response);
         }
       });
