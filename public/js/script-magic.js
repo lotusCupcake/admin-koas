c568ccc0b791c9d71216bf72d4785c9ab055df65
archy7seven <august.ivan20@gmail.com>
2022-02-26 09:43:04 +0700

    penilaian nested

--- public/js/script-magic.js
+++ public/js/script-magic.js
@@ -34,6 +34,11 @@ function initailizeSelect2() {
             type: "post",
             dataType: 'json',
             delay: 250,
+            data: function (params) {
+                return {
+                    searchTerm: params.term // search term
+                };
+            },
             processResults: function (response) {
                 return {
                     results: response
