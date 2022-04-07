85f7d5f404f4ab1d334ad52d161719d6dd291748
fikriansari <fikriansari.mfa@gmail.com>
2022-04-07 09:21:50 +0700

    update version

--- app/Views/layout/templateFooter.php
+++ app/Views/layout/templateFooter.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
   <div class="footer-left">
-    Copyright &copy; 2021 <a href="https://umsu.ac.id/">UMSU</a> Version 1.0.10
+    Copyright &copy; 2021 <a href="https://umsu.ac.id/">UMSU</a> Version 1.0.11
   </div>
 </footer>
